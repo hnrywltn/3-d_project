0dc8f523c99ee62cb5358b855e8cda878d951845
hnrywltn <hnrywltn@gmail.com>
2023-07-01 10:11:50 -0700

    july1st

--- api_pract/demo.py
+++ api_pract/demo.py
@@ -1,12 +1,36 @@
 import requests
 
 url = "https://dog.ceo/api/breeds/list/all"
+random = "https://dog.ceo/api/breeds/image/random"
 
 response = requests.get(url)
+# response = requests.get(random)
+
+# if response.status_code == 200:
+#     data = response.json()
+#     # Process the data here
+#     print(data["message"])
+# else:
+#     print("Error: ", response.status_code)
+
+
+
 
 if response.status_code == 200:
     data = response.json()
     # Process the data here
-    print(type(data))
+    dogs = data
+    for dogsreader in dogs['message']:
+        if dogsreader.startswith('a'):
+            print(dogsreader)
 else:
     print("Error: ", response.status_code)
+
+
+
+# dogs = {'message':
+#  {'affenpinscher': [],
+#   'african': [],
+#   'airedale': [],
+#   'akita': [], 'appenzeller': [], 'australian': ['shepherd'], 'basenji': [], 'beagle': [], 'bluetick': [], 'borzoi': [], 'bouvier': [], 'boxer': [], 'brabancon': [], 'briard': [], 'buhund': ['norwegian'], 'bulldog': ['boston', 'english', 'french'], 'bullterrier': ['staffordshire'], 'cattledog': ['australian'], 'chihuahua': [], 'chow': [], 'clumber': [], 'cockapoo': [], 'collie': ['border'], 'coonhound': [], 'corgi': ['cardigan'], 'cotondetulear': [], 'dachshund': [], 'dalmatian': [], 'dane': ['great'], 'deerhound': ['scottish'], 'dhole': [], 'dingo': [], 'doberman': [], 'elkhound': ['norwegian'], 'entlebucher': [], 'eskimo': [], 'finnish': ['lapphund'], 'frise': ['bichon'], 'germanshepherd': [], 'greyhound': ['italian'], 'groenendael': [], 'havanese': [], 'hound': ['afghan', 'basset', 'blood', 'english', 'ibizan', 'plott', 'walker'], 'husky': [], 'keeshond': [], 'kelpie': [], 'komondor': [], 'kuvasz': [], 'labradoodle': [], 'labrador': [], 'leonberg': [], 'lhasa': [], 'malamute': [], 'malinois': [], 'maltese': [], 'mastiff': ['bull', 'english', 'tibetan'], 'mexicanhairless': [], 'mix': [], 'mountain': ['bernese', 'swiss'], 'newfoundland': [], 'otterhound': [], 'ovcharka': ['caucasian'], 'papillon': [], 'pekinese': [], 'pembroke': [], 'pinscher': ['miniature'], 'pitbull': [], 'pointer': ['german', 'germanlonghair'], 'pomeranian': [], 'poodle': ['medium', 'miniature', 'standard', 'toy'], 'pug': [], 'puggle': [], 'pyrenees': [], 'redbone': [], 'retriever': ['chesapeake', 'curly', 'flatcoated', 'golden'], 'ridgeback': ['rhodesian'], 'rottweiler': [], 'saluki': [], 'samoyed': [], 'schipperke': [], 'schnauzer': ['giant', 'miniature'], 'segugio': ['italian'], 'setter': ['english', 'gordon', 'irish'], 'sharpei': [], 'sheepdog': ['english', 'shetland'], 'shiba': [], 'shihtzu': [], 'spaniel': ['blenheim', 'brittany', 'cocker', 'irish', 'japanese', 'sussex', 'welsh'], 'spitz': ['japanese'], 'springer': ['english'], 'stbernard': [], 'terrier': ['american', 'australian', 'bedlington', 'border', 'cairn', 'dandie', 'fox', 'irish', 'kerryblue', 'lakeland', 'norfolk', 'norwich', 'patterdale', 'russell', 'scottish', 'sealyham', 'silky', 'tibetan', 'toy', 'welsh', 'westhighland', 'wheaten', 'yorkshire'], 'tervuren': [], 'vizsla': [], 'waterdog': ['spanish'], 'weimaraner': [], 'whippet': [], 'wolfhound': ['irish']
+#   }, 'status': 'success'}
---
+++ api_pract/py_playground.py
@@ -0,0 +1,44 @@
+#playing around
+# testvariable = 8
+# lr = list(range(testvariable))
+# print(lr[-1])
+
+
+# coll=[11,222,3333]
+# i=0
+
+# while(i < len(coll)):
+#     print(i)
+#     i = i+1
+
+
+
+
+# #Create a function that returns the string
+# #"Burp" with the amount of "r's" determined
+# #by the input parameters of the function.
+# # range(num[-1])
+# def long_burp(num):
+#     burp = "Bu" + (num * "r") + "p"
+#     return burp
+
+
+# print(long_burp(3))  #> "Burrrp"
+# print(long_burp(5))  #> "Burrrrrp"
+# print(long_burp(9))  #> "Burrrrrrrrrp"
+
+
+
+
+
+# #Create a function(increment) that takes a
+# #number as an argument, increments the number
+# #by +1 and returns the result.
+# def increment(num):
+#     newnum = num + 1
+#     return newnum
+
+
+# print(increment(0))   #> 1
+# print(increment(9))   #> 10
+# print(increment(-3))  #> -2
