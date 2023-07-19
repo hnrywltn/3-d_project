81c487e4552de233a5b7efb7cb115e2346332108
Roman G <romantitanattack@gmail.com>
2023-07-19 13:07:38 -0500

    july 19 after

--- api_pract/py_playground.py
+++ api_pract/py_playground.py
@@ -3,6 +3,15 @@
 # appears consecutively in the given string.
 
 def dynamic_duos(string):
+    count = 0
+    for i in range(len(string) - 1):
+        letter1 = string[i]
+        letter2 = string[i + 1]
+        if letter1 == letter2:
+            count+= 1
+    return count
+
+
     # your code here
 
 
@@ -12,13 +21,15 @@ print(dynamic_duos('hoooraay'))     # 3
 print(dynamic_duos('dinosaurs'))    # 0
 print(dynamic_duos('e'))            # 0
 
+print('_____________________________________________________')
 
 
 
 
 
 
-# Writen a function potent_product that accepts a list of numbers and
+
+# Write a function potent_product that accepts a list of numbers and
 # a product as arguments. The function should return a boolean indicating
 # whether or not a pair of distinct elements in the list result in the
 # product when multiplied together. You may assume that the input list
@@ -26,15 +37,24 @@ print(dynamic_duos('e'))            # 0
 
 def potent_product(numbers, product):
     # your code here
+    for u in range(len(numbers)):
+        x = numbers[u]
+        for a in range(len(numbers)):
+            y = numbers[a]
+            if x * y == product and u != a:
+                return True
+    return False
+
 
 print(potent_product([4, 2, 5, 8], 16))    # True
-print(potent_product([8, 1, 9, 3], 8))     # True
+print(potent_product([8, 9, 1, 3], 8))     # True
 print(potent_product([3, 4], 12))          # True
 print(potent_product([3, 4, 6, 2, 5], 12)) # True
 print(potent_product([4, 2, 5, 7], 16))    # False
 print(potent_product([8, 4, 9, 3], 8))     # False
 print(potent_product([3], 12))             # False
 
+print('_____________________________________________________')
 
 
 
@@ -48,15 +68,28 @@ print(potent_product([3], 12))             # False
 
 def savvy_sums(numbers):
     # your code here
+    count = 0
+    for i in range(len(numbers)):
+        a = numbers[i]
+        for j in range(len(numbers)):
+            b = numbers[j]
+            if a + b == 0 and j > i:
+                count += 1
+    return count
 
+
+# count = 0 - 1 - 2 - 3 - 4
+#                               i
+#                 j
 print(savvy_sums([2, -3, 3, 4, -2]))     # 2
+
 print(savvy_sums([42, 3, -1, -42]))      # 1
 print(savvy_sums([-5, 5]))               # 1
 print(savvy_sums([19, 6, -3, -20]))      # 0
 print(savvy_sums([9]))                   # 0
 
 
-
+print('_____________________________________________________')
 
 
 
@@ -68,6 +101,14 @@ print(savvy_sums([9]))                   # 0
 
 def rational_repeats(string, d):
     # your code here
+    word = ''
+    for i in range(len(string)):
+        if string[i] in d:
+            word += string[i] * d[string[i]]
+        else:
+            word += string[i]
+    return word
+    # ( :
 
 print(rational_repeats('taco', {'a':3, 'c':2}))
 # 'taaacco'
@@ -80,7 +121,7 @@ print(rational_repeats('faarm', {'e':3, 'a':2}))
 
 
 
-
+print('_____________________________________________________')
 
 
 
@@ -131,157 +172,157 @@ print(cryptic_conversion('Our family flew to France')) # "Ouryay amilyfay ewflay
 
 
 
-#playing around
-# testvariable = 8
-# lr = list(range(testvariable))
-# print(lr[-1])
+# #playing around
+# # testvariable = 8
+# # lr = list(range(testvariable))
+# # print(lr[-1])
 
 
-# coll=[11,222,3333]
-# i=0
+# # coll=[11,222,3333]
+# # i=0
 
-# while(i < len(coll)):
-#     print(i)
-#     i = i+1
-
-
-
-
-# #Create a function that returns the string
-# #"Burp" with the amount of "r's" determined
-# #by the input parameters of the function.
-# # range(num[-1])
-# def long_burp(num):
-#     burp = "Bu" + (num * "r") + "p"
-#     return burp
-
-
-# print(long_burp(3))  #> "Burrrp"
-# print(long_burp(5))  #> "Burrrrrp"
-# print(long_burp(9))  #> "Burrrrrrrrrp"
+# # while(i < len(coll)):
+# #     print(i)
+# #     i = i+1
 
 
 
 
+# # #Create a function that returns the string
+# # #"Burp" with the amount of "r's" determined
+# # #by the input parameters of the function.
+# # # range(num[-1])
+# # def long_burp(num):
+# #     burp = "Bu" + (num * "r") + "p"
+# #     return burp
 
-# #Create a function(increment) that takes a
-# #number as an argument, increments the number
-# #by +1 and returns the result.
-# def increment(num):
-#     newnum = num + 1
-#     return newnum
 
-
-# print(increment(0))   #> 1
-# print(increment(9))   #> 10
-# print(increment(-3))  #> -2
+# # print(long_burp(3))  #> "Burrrp"
+# # print(long_burp(5))  #> "Burrrrrp"
+# # print(long_burp(9))  #> "Burrrrrrrrrp"
 
 
 
 
 
+# # #Create a function(increment) that takes a
+# # #number as an argument, increments the number
+# # #by +1 and returns the result.
+# # def increment(num):
+# #     newnum = num + 1
+# #     return newnum
 
 
-
-# __________________________________________________________________
-
-# In this challenge, a farmer is asking you to
-#tell him how many legs can be counted among all
-#his animals. The farmer breeds three species, chickens have
-#two legs, cows have four legs, and pigs have four legs. The
-#farmer has counted his animals and he gives you a subtotal for each species.
-#You have to implement a function that returns the total
-#number of legs of all the animals.
-
-#your code here
-def how_many_legs(num1, num2, num3):
-    sum = (num1 * 2) + (num2 * 4) + (num3 * 4)
-    return sum
-print(how_many_legs(2, 3, 5))    #> 36
-print(how_many_legs(1, 2, 3))    #> 22
-print(how_many_legs(5, 2, 8))    #> 50
-
-# __________________________________________________________________
+# # print(increment(0))   #> 1
+# # print(increment(9))   #> 10
+# # print(increment(-3))  #> -2
 
 
 
 
 
-print("-")
+
+
+
+# # __________________________________________________________________
+
+# # In this challenge, a farmer is asking you to
+# #tell him how many legs can be counted among all
+# #his animals. The farmer breeds three species, chickens have
+# #two legs, cows have four legs, and pigs have four legs. The
+# #farmer has counted his animals and he gives you a subtotal for each species.
+# #You have to implement a function that returns the total
+# #number of legs of all the animals.
+
+# #your code here
+# def how_many_legs(num1, num2, num3):
+#     sum = (num1 * 2) + (num2 * 4) + (num3 * 4)
+#     return sum
+# print(how_many_legs(2, 3, 5))    #> 36
+# print(how_many_legs(1, 2, 3))    #> 22
+# print(how_many_legs(5, 2, 8))    #> 50
+
+# # __________________________________________________________________
 
 
 
 
 
-# __________________________________________________________________
-
-
-# Create a function that takes two numbers as arguments and return their sum.
-
-#your code here
-def addition(nu1, nu2):
-    sumq2 = nu1 + nu2
-    return sumq2
-print(addition(2, 3))   #> 5
-print(addition(-3, -6)) #> -9
-print(addition(7, 3))   #> 10
+# print("-")
 
 
 
-# __________________________________________________________________
 
 
-print("-")
+# # __________________________________________________________________
 
-# __________________________________________________________________
-# Given two strings, first_name and last_name, return a single
-# string in the format "last, first".
 
-#your code here
-def concat_name(fst, lst):
-    endname = lst + ", " + fst
-    return endname
-print(concat_name("First", "Last"))  #> "Last, First"
-print(concat_name("John", "Doe"))    #> "Doe, John"
-print(concat_name("Mary", "Jane"))   #> "Jane, Mary"
-# __________________________________________________________________
+# # Create a function that takes two numbers as arguments and return their sum.
+
+# #your code here
+# def addition(nu1, nu2):
+#     sumq2 = nu1 + nu2
+#     return sumq2
+# print(addition(2, 3))   #> 5
+# print(addition(-3, -6)) #> -9
+# print(addition(7, 3))   #> 10
 
 
 
-print("-")
+# # __________________________________________________________________
+
+
+# print("-")
+
+# # __________________________________________________________________
+# # Given two strings, first_name and last_name, return a single
+# # string in the format "last, first".
+
+# #your code here
+# def concat_name(fst, lst):
+#     endname = lst + ", " + fst
+#     return endname
+# print(concat_name("First", "Last"))  #> "Last, First"
+# print(concat_name("John", "Doe"))    #> "Doe, John"
+# print(concat_name("Mary", "Jane"))   #> "Jane, Mary"
+# # __________________________________________________________________
 
 
 
-# __________________________________________________________________
-#Create a function that takes a string (a random name).
-#If the last character of the name is an "n", return True, otherwise return False.
-
-#your code here
-# def is_last_character_n(nam):
-#    if nam.endswith()
-#     return isn
-# print(is_last_character_n("Aiden"))  #> True
-# print(is_last_character_n("Piet"))   #> False
-# print(is_last_character_n("Bert"))   #> False
-# print(is_last_character_n("Dean"))   #> True
-# __________________________________________________________________
+# print("-")
 
 
-print('-')
+
+# # __________________________________________________________________
+# #Create a function that takes a string (a random name).
+# #If the last character of the name is an "n", return True, otherwise return False.
+
+# #your code here
+# # def is_last_character_n(nam):
+# #    if nam.endswith()
+# #     return isn
+# # print(is_last_character_n("Aiden"))  #> True
+# # print(is_last_character_n("Piet"))   #> False
+# # print(is_last_character_n("Bert"))   #> False
+# # print(is_last_character_n("Dean"))   #> True
+# # __________________________________________________________________
 
 
-# __________________________________________________________________
-#Create a function that takes a list of
-#elements and return the first and last elements as a new list.
+# print('-')
 
-#your code here
-def first_last(fl):
-    sorted = [(fl[0]), (fl[-1])]
-    return sorted
 
-print(first_last([5, 10, 15, 20, 25]))            #> [5, 25]
-print(first_last([13, None, False, True]))        #> [13, True]
-print(first_last([None, 4, "6", "hello", None]))  #> [None, None]
+# # __________________________________________________________________
+# #Create a function that takes a list of
+# #elements and return the first and last elements as a new list.
 
-# surprisingly, this part was easier than the last prompt, leading to a weird error?? idk i cant find out how to fix it
-# __________________________________________________________________
+# #your code here
+# def first_last(fl):
+#     sorted = [(fl[0]), (fl[-1])]
+#     return sorted
+
+# print(first_last([5, 10, 15, 20, 25]))            #> [5, 25]
+# print(first_last([13, None, False, True]))        #> [13, True]
+# print(first_last([None, 4, "6", "hello", None]))  #> [None, None]
+
+# # surprisingly, this part was easier than the last prompt, leading to a weird error?? idk i cant find out how to fix it
+# # __________________________________________________________________
